35e2d6335d62dc6b6a43339871a12a387455b83d
luchian-andrei <luchianandrei116@gmail.com>
2024-10-31 23:05:26 +0200

    update: minor changes;trying to deploy on vercel

--- server/app.js
+++ server/app.js
@@ -5,19 +5,19 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 1166;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// app.use(
-//   cors({
-//     origin: ["http://localhost:3000"],
-//     methods: ["POST", "GET", "PUT"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: ["https://undoo-shop-api.vercel.app/"],
+    methods: ["POST", "GET", "PUT"],
+    credentials: true,
+  })
+);
 
 const router = require("./router.js");
 app.use(router);
